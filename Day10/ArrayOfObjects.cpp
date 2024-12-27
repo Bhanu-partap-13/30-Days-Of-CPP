00ab34b6611a91d74b8042437fba64b0f285dabd
{Bhanu <partapbhanu516@gmail.com>
2024-12-27 12:14:04 +0530

    Day10

--- Day10/ArrayOfObjects.cpp
+++ Day10/ArrayOfObjects.cpp
@@ -92,7 +92,11 @@ public:
 };
 
 int main() {
-    Student students[3] = { Student("Alice", 20), Student("Bob", 21), Student("Charlie", 22) };
+    Student students[3] = 
+    {
+    Student ("Alice", 20),
+    Student("Bob", 21), 
+    Student("Charlie", 22) };
 
     for (int i = 0; i < 3; ++i) {
         students[i].display();
